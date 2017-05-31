69a8e3db62499160e6345b64eacfe484b4a92a5d
Rei Yamaguchi <r-yamaguchi@froide.co.jp>
2017-05-31 11:42:29 +0900

    Initial Commit

---
+++ wifi_connection/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  wifi_connection
+//
+//  Created by FNDM04 on 2017/05/31.
+//  Copyright © 2017年 froide.co.jp. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
