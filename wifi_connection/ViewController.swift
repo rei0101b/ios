799b84bb9571c1a0c90e407c13c227c6c20cb70a
Rei Yamaguchi <r-yamaguchi@froide.co.jp>
2017-06-07 14:29:21 +0900

    fix: リファクタリング
    不要な行の削除

--- wifi_connection/ViewController.swift
+++ wifi_connection/ViewController.swift
@@ -200,25 +200,20 @@ class ViewController: UIViewController {
     
     // 現在接続中のWifiのSSIDを取得
     func getSSID() -> String? {
-        
         let interfaces = CNCopySupportedInterfaces()
         if interfaces == nil {
             return nil
         }
-        
         let interfacesArray = interfaces as! [String]
         if interfacesArray.count <= 0 {
             return nil
         }
-        
         let interfaceName = interfacesArray[0] as String
         let unsafeInterfaceData = CNCopyCurrentNetworkInfo(interfaceName as CFString)
         if unsafeInterfaceData == nil {
             return nil
         }
-        
         let interfaceData = unsafeInterfaceData as! Dictionary <String,AnyObject>
-        
         return interfaceData["SSID"] as? String
     }
     
